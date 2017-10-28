a5e7dbd592ecf3381236874096927e621a7be1c8
Sverchkov Nikolay <ssnikolay@gmail.com>
2017-10-28 18:38:42 +0300

    Testing assocciation clone

--- lib/clowne/active_record_adapter/adapter.rb
+++ lib/clowne/active_record_adapter/adapter.rb
@@ -1,6 +1,12 @@
 module Clowne
   module ActiveRecordAdapter
     class Adapter < Clowne::BaseAdapter::Adapter
+      RESOLVERS = {
+        Clowne::Declarations::IncludeAssociation => Clowne::ActiveRecordAdapter::CloneAssociation,
+        Clowne::Declarations::Nullify => Clowne::BaseAdapter::Nullify,
+        Clowne::Declarations::Finalize => Clowne::BaseAdapter::Finalize,
+      }.freeze
+
       class << self
         def reflections_for(source)
           source.class.reflections
@@ -13,11 +19,7 @@ module Clowne
         private
 
         def resolver(declaration_class)
-          {
-            Clowne::Declarations::IncludeAssociation => Clowne::ActiveRecordAdapter::CloneAssociation,
-            Clowne::Declarations::Nullify => Clowne::BaseAdapter::Nullify,
-            Clowne::Declarations::Finalize => Clowne::BaseAdapter::Finalize,
-          }
+          RESOLVERS.fetch(declaration_class)
         end
       end
     end
--- spec/clowne/active_record_adapter/adapter_spec.rb
+++ spec/clowne/active_record_adapter/adapter_spec.rb
@@ -1,5 +1,4 @@
 RSpec.describe Clowne::ActiveRecordAdapter::Adapter do
-  # subject { described_class.call(source, record, declaration) }
   describe '.reflections_for' do
     subject { described_class.reflections_for(Account.new) }
 
@@ -12,5 +11,28 @@ RSpec.describe Clowne::ActiveRecordAdapter::Adapter do
     subject { described_class.plain_clone(account).as_json }
 
     it { is_expected.to eq(account.dup.as_json) }
+  end
+
+  describe '.clone' do
+    let!(:account) { Account.create(title: 'Some title') }
+    let!(:history) { History.create(some_stuff: 'Some stuff', account: account) }
+
+    subject { described_class.clone(plan, account) }
+
+    context 'when plan is empty' do
+      let(:plan) { [] }
+
+      it { expect(subject.as_json).to eq(Account.new(title: 'Some title').as_json) }
+    end
+
+    context 'when plan has association directive' do
+      let(:plan) { [
+        Clowne::Declarations::IncludeAssociation.new(:history)
+      ] }
+
+      it 'duplicate history' do
+        expect(subject.history.as_json).to eq(History.new(some_stuff: 'Some stuff').as_json)
+      end
+    end
   end
 end
