f98e21c5246ef2231114d8bfa96f830fdbeb41fa
Sverchkov Nikolay <ssnikolay@gmail.com>
2017-11-21 21:05:44 +0300

    Configure base Sequel models; PoC

--- spec/support/sequel/models.rb
+++ spec/support/sequel/models.rb
@@ -1,10 +1,38 @@
-Sequel::Model.plugin :timestamps
+Sequel::Model.plugin :timestamps, force: true, update_on_create: true
 
-class Car < Sequel::Model
-end
+module Sequel
+  class Topic < Sequel::Model
+    one_to_many :posts
+  end
 
-class Racer < Sequel::Model
-end
+  class User < Sequel::Model
+    one_to_many :posts, key: :owner_id
+  end
 
-class Track < Sequel::Model
+  class Post < Sequel::Model
+    many_to_one :topic
+    many_to_one :owner, class_name: Sequel::User
+    one_to_one :account
+
+    dataset_module do
+      def about_animals
+        filter(title: 'animals')
+      end
+    end
+    # one_to_one :history, through: :account
+    # has_and_belongs_to_many :tags
+  end
+
+  class Account < Sequel::Model
+    many_to_one :post
+    # has_one :history
+  end
+  #
+  # class History < Sequel::Model
+  #   belongs_to :account
+  # end
+  #
+  # class Tag < Sequel::Model
+  #   has_and_belongs_to_many :posts
+  # end
 end
