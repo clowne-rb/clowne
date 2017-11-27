c4455f73205b22f0d6432f12a84ff543df9416bd
Sverchkov Nikolay <ssnikolay@gmail.com>
2017-11-27 21:14:19 +0300

    Implement FactoryBot for Sequel models

--- spec/support/sequel/schema.rb
+++ spec/support/sequel/schema.rb
@@ -24,8 +24,31 @@ SEQUEL_DB.create_table :posts do
   DateTime :updated_at, null: false
 end
 
+SEQUEL_DB.create_table :tags do
+  primary_key :id
+  String :value
+  DateTime :created_at, null: false
+  DateTime :updated_at, null: false
+end
+
+SEQUEL_DB.create_table :posts_tags do
+  primary_key :id
+  Integer :post_id
+  Integer :tag_id
+end
+
 SEQUEL_DB.create_table :accounts do
   primary_key :id
   Integer :post_id
   String :title
+  DateTime :created_at, null: false
+  DateTime :updated_at, null: false
+end
+
+SEQUEL_DB.create_table :histories do
+  primary_key :id
+  Integer :account_id
+  String :some_stuff
+  DateTime :created_at, null: false
+  DateTime :updated_at, null: false
 end
