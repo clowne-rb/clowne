89040f6aebf9284db438633b1c736b6bd6c2f650
Sverchkov Nikolay <ssnikolay@gmail.com>
2017-12-05 18:58:46 +0300

    DRY association layer

--- lib/clowne/adapters/active_record/associations/base.rb
+++ lib/clowne/adapters/active_record/associations/base.rb
@@ -1,65 +1,21 @@
 # frozen_string_literal: true
 
+require 'clowne/adapters/base/association'
+
 module Clowne
   module Adapters # :nodoc: all
     class ActiveRecord
       module Associations
-        class Base
-          # Params:
-          # +reflection+:: Association eflection object
-          # +source+:: Instance of cloned object (ex: User.new(posts: posts))
-          # +declaration+:: = Relation description
-          #                   (ex: Clowne::Declarations::IncludeAssociation.new(:posts))
-          # +params+:: = Instance of Hash
-          def initialize(reflection, source, declaration, params)
-            @source = source
-            @scope = declaration.scope
-            @clone_with = declaration.clone_with
-            @params = params
-            @association_name = declaration.name.to_s
-            @reflection = reflection
-            @cloner_options = params
-            @cloner_options.merge!(traits: declaration.traits) if declaration.traits
-          end
-
-          def call(_record)
-            raise NotImplementedError
-          end
-
-          def association
-            @_association ||= source.__send__(association_name)
-          end
-
-          def clone_one(child)
-            cloner = cloner_for(child)
-            cloner ? cloner.call(child, cloner_options) : clone_record(child)
-          end
-
-          def with_scope
-            base_scope = association
-            if scope.is_a?(Symbol)
-              base_scope.__send__(scope)
-            elsif scope.is_a?(Proc)
-              base_scope.instance_exec(params, &scope) || base_scope
-            else
-              base_scope
-            end
-          end
-
+        class Base < Base::Association
           private
 
           def clone_record(record)
             Clowne.resolve_adapter(:active_record).copier.call(record)
           end
 
-          def cloner_for(child)
-            return clone_with if clone_with
-
-            return child.class.cloner_class if child.class.respond_to?(:cloner_class)
+          def init_scope
+            association
           end
-
-          attr_reader :source, :scope, :clone_with, :params, :association_name,
-                      :reflection, :cloner_options
         end
       end
     end
--- lib/clowne/adapters/sequel/associations/base.rb
+++ lib/clowne/adapters/sequel/associations/base.rb
@@ -1,55 +1,20 @@
 # frozen_string_literal: true
 
+require 'clowne/adapters/base/association'
+
 module Clowne
   module Adapters # :nodoc: all
     class Sequel
       module Associations
-        class Base
-          # Params:
-          # +reflection+:: Association eflection object
-          # +source+:: Instance of cloned object (ex: User.new(posts: posts))
-          # +declaration+:: = Relation description
-          #                   (ex: Clowne::Declarations::IncludeAssociation.new(:posts))
-          # +params+:: = Instance of Hash
-          def initialize(reflection, source, declaration, params)
-            @source = source
-            @scope = declaration.scope
-            @clone_with = declaration.clone_with
-            @params = params
-            @association_name = declaration.name.to_s
-            @reflection = reflection
-            @cloner_options = params
-            @cloner_options.merge!(traits: declaration.traits) if declaration.traits
-          end
-
-          def call(_record)
-            raise NotImplementedError
-          end
-
-          def association
-            @_association ||= source.__send__(association_name)
-          end
-
-          def clone_one(child)
-            cloner = cloner_for(child)
-            cloner ? cloner.call(child, cloner_options) : clone_record(child)
-          end
-
-          def with_scope
-            base_scope = association_dataset
-            if scope.is_a?(Symbol)
-              base_scope.__send__(scope)
-            elsif scope.is_a?(Proc)
-              base_scope.instance_exec(params, &scope) || base_scope
-            else
-              base_scope
-            end.to_a
-          end
-
+        class Base < Base::Association
           private
 
           def clone_record(record)
             Clowne.resolve_adapter(:sequel).copier.call(record)
+          end
+
+          def init_scope
+            @_init_scope ||= source.__send__([association_name, 'dataset'].join('_'))
           end
 
           def for_clonable(record)
@@ -71,31 +36,20 @@ module Clowne
 
           # TODO: think how to get rid of many conversion
           def clonable_attributes(record, prev_assoc = nil)
-            record.associations.each_with_object(record.dup.to_hash) do |(name, value), memo|
+            record.associations.each_with_object(record.to_hash) do |(name, value), memo|
               next if !prev_assoc.nil? && value.is_a?(prev_assoc)
 
-              association_hash =
-                if value.is_a?(Array)
-                  value.map { |v| clonable_attributes(v, record.class) }
-                else
-                  clonable_attributes(value, record.class)
-                end
-              memo["#{name}_attributes"] = association_hash
+              memo["#{name}_attributes"] = clone_association(record.class, value)
             end
           end
 
-          def cloner_for(child)
-            return clone_with if clone_with
-
-            return child.class.cloner_class if child.class.respond_to?(:cloner_class)
+          def clone_association(record_class, assoc)
+            if assoc.is_a?(Array)
+              assoc.map { |v| clonable_attributes(v, record_class) }
+            else
+              clonable_attributes(assoc, record_class)
+            end
           end
-
-          def association_dataset
-            @_association_dataset ||= source.__send__([association_name, 'dataset'].join('_'))
-          end
-
-          attr_reader :source, :scope, :clone_with, :params, :association_name,
-                      :reflection, :cloner_options
         end
       end
     end
