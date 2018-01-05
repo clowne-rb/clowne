2defeb4e10d0ce3fc24e1946596831b6e5d4c1f9
Vladimir Dementyev <dementiev.vm@gmail.com>
2018-01-05 20:30:56 +0300

    Make more meaningful warnings

--- lib/clowne/adapters/active_record/associations/has_one.rb
+++ lib/clowne/adapters/active_record/associations/has_one.rb
@@ -8,7 +8,12 @@ module Clowne
           def call(record)
             child = association
             return record unless child
-            warn '[Clowne] Has one association does not support scopes' unless scope.nil?
+            unless scope.nil?
+              warn(
+                "[Clowne] Has one association does not support scopes " \
+                "(#{@association_name} for #{@source.class})"
+              )
+            end
 
             child_clone = clone_one(child)
             child_clone[:"#{reflection.foreign_key}"] = nil
