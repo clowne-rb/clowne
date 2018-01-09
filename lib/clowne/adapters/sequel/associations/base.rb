6f530a8d5bcb9ee9b65f99d4afc3772d8b2f3926
Sverchkov Nikolay <ssnikolay@gmail.com>
2018-01-09 17:56:18 +0300

    Code review fixes: use copier logic directly in associations

--- lib/clowne/adapters/sequel/associations/base.rb
+++ lib/clowne/adapters/sequel/associations/base.rb
@@ -10,7 +10,7 @@ module Clowne
           private
 
           def clone_record(record)
-            Clowne.resolve_adapter(:sequel).dup_source(record)
+            Clowne::Adapters::Sequel::Copier.call(record)
           end
 
           def init_scope
