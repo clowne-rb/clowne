de3cabcced41e66466a2bde8a73ceed41be1fb44
Sverchkov Nikolay <ssnikolay@gmail.com>
2018-01-12 10:47:17 +0300

    Change brand color

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -33,7 +33,7 @@ const siteConfig = {
   favicon: 'img/favicon.png',
   /* colors for website */
   colors: {
-    primaryColor: '#7d638e', /* #2E8555 */
+    primaryColor: '#0169a4', /* #2E8555 */
     secondaryColor: '#205C3B',
   },
   // This copyright info is used in /core/Footer.js and blog rss/atom feeds.
