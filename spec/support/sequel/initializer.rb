3d69a2a8ff119e8dcd0cc78e93bbae99fcedaaa1
Sverchkov Nikolay <ssnikolay@gmail.com>
2018-01-15 22:41:56 +0300

    Configure Sequel for sqlite3 and JRuby

--- spec/support/sequel/initializer.rb
+++ spec/support/sequel/initializer.rb
@@ -1,6 +1,12 @@
 require 'sequel'
+db =
+  if RUBY_PLATFORM =~ /java/
+    'jdbc:sqlite::memory:'
+  else
+    'sqlite::memory:'
+  end
 
-SEQUEL_DB = Sequel.sqlite
+SEQUEL_DB = Sequel.connect(db)
 
 require_relative './schema.rb'
 require_relative './models.rb'
