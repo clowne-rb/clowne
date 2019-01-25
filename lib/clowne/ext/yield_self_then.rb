fd178602e29b9a1d0faf24b30a9d9b984f9e0eac
Nikolay Sverchkov <ssnikolay@gmail.com>
2019-01-25 19:52:14 +0300

    Use then+yield_self refinements

---
+++ lib/clowne/ext/yield_self_then.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Clowne
+  module Ext # :nodoc: all
+    # Add yield_self and then if missing
+    module YieldSelfThen
+      module Ext
+        unless nil.respond_to?(:yield_self)
+          def yield_self
+            yield self
+          end
+        end
+
+        alias then yield_self
+      end
+
+      # See https://github.com/jruby/jruby/issues/5220
+      ::Object.include(Ext) if RUBY_PLATFORM =~ /java/i
+
+      refine Object do
+        include Ext
+      end
+    end
+  end
+end
